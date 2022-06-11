599dd840cddd6f44a84d8b33aa2c4f76b029c1a8
Nandini Khandelwal <nandinikhandelwal20.set@modyuniversity.ac.in>
2022-06-12 02:43:59 +0530

    New changes

--- pdf.js
+++ pdf.js
@@ -1,7 +1,15 @@
 function generatePdf() {
     const element = document.querySelector('.generate');
 
+    var opt = {
+        margin: 0,
+        filename: 'myfile.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+    };
+
     html2pdf()
-        .from(element)
+        .set(opt).from(element)
         .save();
 };
